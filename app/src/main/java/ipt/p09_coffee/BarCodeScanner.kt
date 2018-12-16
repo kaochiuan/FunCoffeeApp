f7d17320d8e3292c9ba9dfa2e18f5600303a3f6a
kaochiuan.teng <kaochiuan.teng@zyxel.com.tw>
2018-12-16 12:59:15 +0800

    scan bar code to popup message or goto hyperlink

--- app/src/main/java/ipt/p09_coffee/BarCodeScanner.kt
+++ app/src/main/java/ipt/p09_coffee/BarCodeScanner.kt
@@ -5,15 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
+import android.widget.PopupWindow
+import android.widget.TextView
 import com.google.zxing.Result
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.layout_popupwindow.view.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
-import org.json.JSONArray
 import org.json.JSONObject
 
 
@@ -27,12 +30,13 @@ class BarCodeScanner : AppCompatActivity(), ZXingScannerView.ResultHandler {
     private lateinit var mScannerView: ZXingScannerView
     private var serverDestination: String? = null
     private var bt_scanner: Button? = null
+    private var popupWindow: PopupWindow? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_code_scanner)
-
+        popupWindow = PopupWindow(this)
         mScannerView = ZXingScannerView(this)
         serverDestination = getString(R.string.server_destination)
         bt_scanner = findViewById(R.id.Btn_id_scanner)
@@ -40,7 +44,6 @@ class BarCodeScanner : AppCompatActivity(), ZXingScannerView.ResultHandler {
     }
 
     private val bt_ScannerListener = View.OnClickListener {
-        //mScannerView = ZXingScannerView(this)
         setContentView(mScannerView)
 
         mScannerView.setResultHandler(this)
@@ -73,10 +76,9 @@ class BarCodeScanner : AppCompatActivity(), ZXingScannerView.ResultHandler {
         val authToken = PreferenceHelper.getAuthToken(this)
         val temp = "Bearer ${authToken.accessToken}"
 
-        val act_context = this
-        var isUrl = false
-        val serial_number = result?.text
-        val url = "$serverDestination/serial_number?serial_number=$serial_number"
+        val actContext = this
+        val serialNumber = result?.text
+        val url = "$serverDestination/serial_number?serial_number=$serialNumber"
         val client = AsyncHttpClient()
         client.addHeader("authorization", temp)
         client.addHeader("Content-Type", "application/json")
@@ -86,36 +88,32 @@ class BarCodeScanner : AppCompatActivity(), ZXingScannerView.ResultHandler {
                 try {
                     val mJMessage = JSONObject(data)
                     val message = mJMessage.getString("customized_message")
-                    if (message.startsWith("http://",ignoreCase = true) or
-                            message.startsWith("https://",ignoreCase = true)){
-                        isUrl = true
-                        openNewTabWindow(urls = message, context = act_context)
+                    if (message.startsWith("http://", ignoreCase = true) or
+                            message.startsWith("https://", ignoreCase = true)) {
+                        openNewTabWindow(urls = message, context = actContext)
+                    } else {
+                        showPopupWindow(message = message)
                     }
                 } catch (err_Mag: Exception) {
-                    Toast.makeText(applicationContext, err_Mag.message, Toast.LENGTH_LONG).show()
-                    isUrl = false
+                    showPopupWindow(message = err_Mag.message!!)
+                    // Toast.makeText(applicationContext, err_Mag.message, Toast.LENGTH_LONG).show()
                 }
             }
 
             override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
-                if (statusCode == 401) {
-                    Toast.makeText(applicationContext, getString(R.string.token_expired), Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(applicationContext, "取得訊息失敗", Toast.LENGTH_LONG).show()
+                when (statusCode) {
+                    401 -> showPopupWindow(message = getString(R.string.token_expired))
+                    404 -> showPopupWindow(message = "Bar code not found, invalid operation")
+                    else -> showPopupWindow(message = "取得訊息失敗")
                 }
             }
         })
-
-        Toast.makeText(this, url, Toast.LENGTH_SHORT).show()
-        if(!isUrl) {
-            openNewTabWindow(urls = url, context = this)
-        }
         //Camera will stop after scanning result, so we need to resume the
         //preview in order scan more codes
-        //mScannerView.resumeCameraPreview(this)
+        mScannerView.resumeCameraPreview(this)
     }
 
-    fun openNewTabWindow(urls: String, context : Context) {
+    fun openNewTabWindow(urls: String, context: Context) {
         val uris = Uri.parse(urls)
         val intents = Intent(Intent.ACTION_VIEW, uris)
         val b = Bundle()
@@ -123,4 +121,29 @@ class BarCodeScanner : AppCompatActivity(), ZXingScannerView.ResultHandler {
         intents.putExtras(b)
         context.startActivity(intents)
     }
+
+    private fun showPopupWindow(message: String) {
+        val popupView = LayoutInflater.from(this).inflate(R.layout.layout_popupwindow, null)
+
+
+        //get txt view from "layout" which will be added into popup window
+        //before it you tried to find view in activity container
+        val txt = popupView.findViewById(R.id.textViewPrivateMessage) as TextView
+        txt.text = message
+
+        // set click listener for ok button
+        popupView.layout_popupwindow_confirmButton.setOnClickListener { view ->
+            popupWindow!!.dismiss()
+        }
+
+        popupWindow!!.contentView = popupView
+        popupWindow!!.width = ViewGroup.LayoutParams.MATCH_PARENT
+        popupWindow!!.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        // disappear popupWindow if touch outside of it
+        popupWindow!!.isOutsideTouchable = true
+
+        // show popWindow
+        popupWindow!!.showAsDropDown(bt_scanner, 0, 800)
+    }
+
 }
